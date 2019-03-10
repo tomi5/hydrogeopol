2b136b0af2ea9ceac0decbc7a7a3af4124012e84
Tomasz <34981728+tomi5@users.noreply.github.com>
2019-03-10 11:19:45 +0100

    Add files via upload

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,5 +1,11 @@
-// show menu on mobile
+// remove class no--js 
 
+$(document).ready(function () {
+	$('body *').removeClass('no--js')
+});
+
+
+// show menu on mobile
 
 function toggleActive() {
 	$('.burger').toggleClass('active');
@@ -32,3 +38,52 @@ $('.btn--header').on('click', function () {
 	}, 1000)
 
 })
+
+
+// animation effect
+
+$(document).on('scroll', function () {
+	const $scrollValue = $(this).scrollTop();
+	const $windowHeight = $(window).height();
+	const $qualitiesFromTop = $('.container--qualities').offset().top;
+	const $listFromTop = $('.service__list').offset().top;
+
+	if ($scrollValue > $qualitiesFromTop - $windowHeight / 2) {
+		$('.container--qualities').addClass('active');
+	}
+	if ($scrollValue > $listFromTop - $windowHeight / 2) {
+		$('.service__list').addClass('active');
+	}
+	if ($scrollValue < 300) {
+		$('.container--qualities').removeClass('active')
+		$('.service__list').removeClass('active'); // czyszczyciel
+	}
+})
+
+
+//menu active possition
+
+function activeMenu() {
+	const $scrollValue = $(window).scrollTop();
+	const $heightAbout = $('#about').offset().top;
+	const $heightQualities = $('.section--qualities').offset().top;
+	const $heightServices = $('#services').offset().top;
+	const $heightContact = $('#contact').offset().top;
+
+	if ($scrollValue < $heightAbout) {
+		$('.nav__item').removeClass('active');
+		$('.nav__item--home').addClass('active');
+	} else if ($scrollValue < $heightQualities) {
+		$('.nav__item').removeClass('active');
+		$('.nav__item--about').addClass('active');
+	} else if ($scrollValue < $heightServices) {
+		$('.nav__item').removeClass('active');
+	} else if ($scrollValue < $heightContact) {
+		$('.nav__item').removeClass('active');
+		$('.nav__item--services').addClass('active');
+	} else {
+		$('.nav__item').removeClass('active');
+		$('.nav__item--contact').addClass('active');
+	}
+}
+$(window).on("scroll", activeMenu)
